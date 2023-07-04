8037dd9d7715710226eddc15c079a45b3a11eb87
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2023-07-04 18:56:31 +0300

    simple mock up page
    Provided a simple web page with log in and sign up options (to be configured later)

---
+++ ResumеBuilder/ResumеBuilder/Pages/LogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ResumеBuilder.Pages
+{
+    public class LogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
