0e8386297d65970966c914e96a3838e27083a736
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2023-07-19 11:58:37 +0300

    erase some stuff

--- ResumеBuilder/ResumеBuilder/obj/Release/net7.0/PubTmp/Out/wwwroot/js/addInfo.js
+++ ResumеBuilder/ResumеBuilder/obj/Release/net7.0/PubTmp/Out/wwwroot/js/addInfo.js
@@ -27,3 +27,65 @@ function RemoveSkillField() {
     skillGroup.removeChild(skillsList[skillsList.length - 1]);
     skillGroup.removeChild(skillBreakList[skillBreakList.length - 1]);
 }
+
+var interestGroup = document.getElementById("interestGroup");
+
+function AddInterestField() {
+    const newInterestField = document.createElement("input");
+
+    newInterestField.type = "text";
+
+    newInterestField.name = "interestField";
+
+    newInterestField.classList.add("form-control");
+
+    newInterestField.classList.add("interest-field");
+
+    const lineBreak = document.createElement("br");
+
+    lineBreak.className = "form-interest-break";
+
+    interestGroup.appendChild(lineBreak);
+    interestGroup.appendChild(newInterestField);
+}
+
+let interestsList = document.getElementsByClassName("interest-field");
+
+let interestBreakList = document.getElementsByClassName("form-interest-break");
+
+function RemoveInterestField() {
+    interestGroup.removeChild(interestsList[interestsList.length - 1]);
+    interestGroup.removeChild(interestBreakList[interestBreakList.length - 1]);
+}
+
+var experienceGroup = document.getElementById("experienceGroup");
+
+function AddExperienceFields() {
+    const mainItem = document.getElementById("mainExperienceSet");
+    const clone = mainItem.cloneNode(true);
+
+    experienceGroup.appendChild(clone);
+}
+
+function RemoveExperienceFields() {
+    let items = document.getElementsByClassName("experienceSet");
+    if (items.length > 1) {
+        experienceGroup.removeChild(items[items.length - 1]);
+    }
+}
+
+var educationGroup = document.getElementById("educationGroup");
+
+function AddEducationFields() {
+    const mainItem = document.getElementById("mainEducationSet");
+    const clone = mainItem.cloneNode(true);
+
+    educationGroup.appendChild(clone);
+}
+
+function RemoveEducationFields() {
+    let items = document.getElementsByClassName("educationSet");
+    if (items.length > 1) {
+        educationGroup.removeChild(items[items.length - 1]);
+    }
+}
